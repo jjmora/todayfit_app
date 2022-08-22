734f6b14040521fb852d66ef27d77ad0073d8bb5
JuanJoseMora <juanmora.dev@gmail.com>
2022-08-22 16:14:15 +0200

    Partner make:crud executed

---
+++ src/Form/PartnerType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Partner;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PartnerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Name')
+            ->add('Email')
+            ->add('Address')
+            ->add('Active')
+            ->add('franchise')
+            ->add('permissions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Partner::class,
+        ]);
+    }
+}
