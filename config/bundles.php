acc1b2aed62e857c146d6226b5495b57e39b095a
JuanJoseMora <juanmora.dev@gmail.com>
2022-08-28 16:09:04 +0200

    Bug in register, it doesn't send the email for verification

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
