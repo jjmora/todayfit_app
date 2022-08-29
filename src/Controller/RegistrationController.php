50da89567cd3d25da9d6aafebec52e043a36149e
JuanJoseMora <juanmora.dev@gmail.com>
2022-08-29 18:14:43 +0200

    .env mailer variable added for mailtrap + registerController modify

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -75,7 +75,8 @@ class RegistrationController extends AbstractController
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+            // $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
+            $this->addFlash('verify_email_error', $exception->getReason());
 
             return $this->redirectToRoute('app_register');
         }
@@ -83,6 +84,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_admin');
     }
 }
