8e1fcb4ef1ef317042759d7986c243480221749a
JuanJoseMora <juanmora.dev@gmail.com>
2022-09-23 13:02:47 +0200

    folder images configured within webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,10 @@ Encore
     //.autoProvidejQuery()
 
     .copyFiles({
-      from: './assets/images'
+      from: './assets/images',
+
+      // optional target path, relative to the output dir
+      to: 'images/[path][name].[ext]'
     })
 ;
 
