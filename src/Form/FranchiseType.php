abfbbe41df5fbe65697d0267c4052d9bb6b6282c
JuanJoseMora <juanmora.dev@gmail.com>
2022-09-27 11:44:59 +0200

    Franchise new Form updated with user selection

--- src/Form/FranchiseType.php
+++ src/Form/FranchiseType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Franchise;
 use App\Entity\Permission;
+use App\Entity\User;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,7 +17,24 @@ class FranchiseType extends AbstractType
     {
         $builder
             ->add('Name')
-            ->add('Email')
+            ->add('Email', null, [
+              'label' => 'Adresse E-mail personnelle'
+            ])
+            ->add('user', EntityType::class, [
+              'class' => User::class,
+              'placeholder' => 'Utilisateurs disponibles',
+              'choice_label' => 'email',
+              'query_builder' => function(EntityRepository $er) {
+                return $er->createQueryBuilder('u')
+                  ->where('u.roles LIKE :role')
+                  ->setParameter('role', '%"'.'ROLE_FRANCHISE'.'"%')
+                  ->leftJoin(Franchise::class, 'f', 'WITH', 'u = f.user' )
+                  ->andwhere('f.user is NULL');
+                ;
+              },
+              'multiple' => false,
+              'expanded' => false
+            ])
             ->add('Active')
             ->add('permissions', EntityType::class, [
               'class' => Permission::class,
