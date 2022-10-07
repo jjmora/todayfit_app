25a7668b71a93c10c4acebc87058d98498f9c41e
JuanJoseMora <juanmora.dev@gmail.com>
2022-10-07 23:43:50 +0200

    Card Front End and data getting integration on Franchise Index

--- assets/app.js
+++ assets/app.js
@@ -5,11 +5,12 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
 
 // or, specify which plugins you need:
 import * as bootstrap from 'bootstrap'
 
 // start the Stimulus application
 import './bootstrap';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
