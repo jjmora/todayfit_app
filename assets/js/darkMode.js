75cd8544768f066438f189b8b1b33a33ba05ca51
JuanJoseMora <juanmora.dev@gmail.com>
2022-10-19 12:07:33 +0200

    Dark mode implemented with basic CSS for testing

---
+++ assets/js/darkMode.js
@@ -0,0 +1,21 @@
+let elements = document.getElementsByClassName('dark-mode')
+
+for (item of elements) {
+  item.addEventListener('click', darkMode)
+}
+
+function darkMode() {
+  let body = document.querySelector('body')
+  let mode = this.dataset.mode
+  body.dataset.theme = mode
+  
+  let moon = document.getElementById("moon")
+  let sun = element = document.getElementById("sun")
+  if(mode === "dark"){
+    moon.classList.toggle("hidden-icon")
+    sun.classList.toggle("hidden-icon")
+  } else {
+    moon.classList.toggle("hidden-icon")
+    sun.classList.toggle("hidden-icon")
+  }
+}
