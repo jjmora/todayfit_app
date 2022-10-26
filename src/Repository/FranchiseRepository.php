a0f2ee91faf14fa9795091c41a90e6e4c8aa3020
JuanJoseMora <juanmora.dev@gmail.com>
2022-10-26 15:59:39 +0200

    Full text search changed to LIKE sql search. Working for Partner and Franchise

--- src/Repository/FranchiseRepository.php
+++ src/Repository/FranchiseRepository.php
@@ -27,8 +27,10 @@ class FranchiseRepository extends ServiceEntityRepository
 
       if($input_data != null){
         $query
-          ->where('MATCH_AGAINST(p.Name, p.Email, p.description) AGAINST (:input_data boolean)>0')
-          ->setParameter('input_data', $input_data)
+          // ->where('MATCH_AGAINST(p.Name, p.Email, p.description) AGAINST (:input_data boolean)>0')
+          // ->setParameter('input_data', $input_data)
+          ->where('p.Name LIKE :input_data OR p.Email LIKE :input_data OR p.description LIKE :input_data')
+          ->setParameter('input_data', '%'.$input_data.'%')
           ;
       }
       if($active != null){
