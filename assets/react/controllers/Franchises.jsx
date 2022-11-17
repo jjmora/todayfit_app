fd5af9af8e83484b31df10f41b5e7ff92c81113d
JuanJoseMora <juanmora.dev@gmail.com>
2022-11-17 14:19:27 +0100

    Bootstrap styling in the react twig file for testing Franchises

--- assets/react/controllers/Franchises.jsx
+++ assets/react/controllers/Franchises.jsx
@@ -18,10 +18,6 @@ const Franchises = (props) => {
         console.log('Initial Data: ', response.data.franchises)
         setAllFranchises(response.data.franchises)
         setFilteredFranchises(response.data.franchises)
-        
-        setFranchises(response.data.franchises)
-        setFilteredByInput(response.data.franchises)
-        setFilterByClick(response.data.franchises)
       })
       .catch((error) => {
         console.log(error);
@@ -74,15 +70,43 @@ const Franchises = (props) => {
           <li onClick={handleClick} id="non-active">Non Active</li>
         </ul>
       </div>
-      <div className='search-input-wrapper'>
+
+      <div className="input-group mb-3">
         <input 
           id="form-input"
           type="search" 
-          className='search-input' 
-          placeholder='Please enter your text here...'
+          className="form-control" 
+          placeholder="Entrez le(s) mot(s) clé(s)..." 
+          aria-label="Username" 
+          aria-describedby="basic-addon1" 
           onChange={handleInputField}
-        ></input>
+        />
+        <span className="input-group-text bg-primary text-white" id="basic-addon1"><i className="bi bi-search"></i></span>
       </div>
+
+      <section>
+        <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
+        {
+          filteredFranchises?.map( (franchise) => {
+            return(
+              <div className="col mb-4">
+                <div className="card h-100">
+                  <img src="https://via.placeholder.com/150" width="150px" className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  </div>
+                  <div className="card-footer">
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+        </div>
+      </section>
+
       <div>
         {
           filteredFranchises?.map( (franchise) => {
