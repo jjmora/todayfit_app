8fe7193b27232510b79fb3e08a12d35f53ee7f91
JuanJoseMora <juanmora.dev@gmail.com>
2023-02-20 17:12:16 +0100

    Filter working well

--- assets/react/controllers/Franchises.jsx
+++ assets/react/controllers/Franchises.jsx
@@ -1,119 +1,158 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Card from './Card'
-import Pagination from './Pagination'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./Card";
+import Pagination from "./Pagination";
 
 const Franchises = () => {
-  const [ franchisesArray, setFranchisesArray ] = useState()
-  const [ filteredFranchises, setFilteredFranchises ] = useState()
-  const [ inputValue, setInputValue ] = useState('')
-  const [ activeState, setActiveState ] = useState('all')
+  const [franchisesArray, setFranchisesArray] = useState();
+  const [filteredFranchises, setFilteredFranchises] = useState();
+  const [inputValue, setInputValue] = useState("");
+  const [activeState, setActiveState] = useState("all");
+  const [errorOnLoading, setErrorOnLoading] = useState();
   //Pagination
-  const [ currentPage, setCurrentPage ] = useState(1);
-  const [ postsPerPage, setPostsPerPage ] = useState(6);
-  const [ filteredItemsQty, setFilteredItemsQty ] = useState(100)
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(6);
+  const [filteredItemsQty, setFilteredItemsQty] = useState(100);
 
-  const baseUrl = 'http://localhost:8000'
+  // const baseUrl = 'http://localhost:8000'
   // const currBaseUrl = window.location.href
 
-  const lastPostIndex = currentPage * postsPerPage // 1 * 6 = 6
-  const firstPostIndex = lastPostIndex - postsPerPage // 6 - 6 = 0
+  let dataByInput;
+  let dataByActiveState;
+
+  const lastPostIndex = currentPage * postsPerPage; // 1 * 6 = 6
+  const firstPostIndex = lastPostIndex - postsPerPage; // 6 - 6 = 0
 
   // Load Data at document loading
-  useEffect( () => {
-    axios.get(`${baseUrl}/franchise/franchise_json`)
-      .then(response => {
-        console.log('Initial Data: ', response.data)
-        setFranchisesArray(response.data.franchisesArray)
-        setFilteredFranchises(response.data.franchisesArray.slice(firstPostIndex, lastPostIndex))
-        setFilteredItemsQty(response.data.franchisesArray.length)
+  useEffect(() => {
+    axios
+      .get(`/franchise/franchise_json`)
+      .then((response) => {
+        //console.log("Initial Data: ", response.data);
+        setFranchisesArray(response.data.franchisesArray);
+        setFilteredFranchises(
+          response.data.franchisesArray.slice(firstPostIndex, lastPostIndex)
+        );
+        setFilteredItemsQty(response.data.franchisesArray.length);
+        setErrorOnLoading();
       })
       .catch((error) => {
-        console.log(error);
-      })
-  }, [])
+        //console.log(error);
+        setErrorOnLoading("Une erreur c'est produite");
+      });
+  }, []);
 
-  const handleClick = (e) => {
-    setActiveState(e.target.id)
-    let parent = e.currentTarget.parentElement
-    console.log(parent)
-    let children = parent.children
-    console.log(children)
-    for(let child of children) {
-      child.classList.remove('filter-active')
+  const handleFilterClick = (e) => {
+    setActiveState(e.target.id);
+    // give CSS classes for formatting
+    let parent = e.currentTarget.parentElement;
+    //console.log(parent);
+    let children = parent.children;
+    //console.log(children);
+    for (let child of children) {
+      child.classList.remove("filter-active");
     }
-    e.target.classList.add('filter-active')
-  }
-  
+    e.target.classList.add("filter-active");
+  };
+
   const handleInputField = (e) => {
-    setInputValue(e.target.value)
-  }
+    setInputValue(e.target.value);
+  };
 
-  useEffect( () => {
-    let dataByInput
-    if(inputValue) {
-      dataByInput = franchisesArray?.filter( (fr, k) => {
-        return ( (fr.email.includes(inputValue)) || (fr.email_perso.includes(inputValue)) || (fr.name.includes(inputValue)))
-      })
+  useEffect(() => {
+    console.log("FilteredFranchises: ", filteredFranchises);
+    // Filter dat by User Input - by inputValue
+
+    if (inputValue) {
+      dataByInput = franchisesArray?.filter((fr, k) => {
+        return (
+          fr.email.includes(inputValue) ||
+          fr.email_perso.includes(inputValue) ||
+          fr.name.includes(inputValue)
+        );
+      });
     } else {
-      dataByInput = franchisesArray
+      dataByInput = franchisesArray;
     }
-    console.log('Data by input:', dataByInput)
+    console.log("Data by input:", dataByInput);
+    console.log("FranchiseArray:", franchisesArray);
 
-    let dataByActiveState
-    if(activeState === 'active'){
-      dataByActiveState = dataByInput?.filter( (fr, k) => {
-        return fr.isActive === true
-      })
-      console.log("X :", dataByActiveState?.length)
-      setFilteredItemsQty(dataByActiveState?.length)
+    // by activeState
+    if (activeState === "active") {
+      dataByActiveState = dataByInput?.filter((fr, k) => {
+        return fr.isActive === true;
+      });
+      console.log("X :", dataByActiveState);
+      console.log("X :", dataByActiveState?.length);
 
-    } else if(activeState === 'non-active') {
-      dataByActiveState = dataByInput?.filter( (fr, k) => {
-        return fr.isActive === false
-      })
-      console.log("X :", dataByActiveState?.length)
-      setFilteredItemsQty(dataByActiveState?.length)
+      setFilteredItemsQty(dataByActiveState?.length);
+    } else if (activeState === "non-active") {
+      dataByActiveState = dataByInput?.filter((fr, k) => {
+        return fr.isActive === false;
+      });
+      console.log("X :", dataByActiveState?.length);
+
+      setFilteredItemsQty(dataByActiveState?.length);
     } else {
-      dataByActiveState = dataByInput
-      console.log("X :", dataByActiveState?.length)
-      setFilteredItemsQty(dataByActiveState?.length)
+      dataByActiveState = dataByInput;
+      console.log("X :", dataByActiveState?.length);
+
+      setFilteredItemsQty(dataByActiveState?.length);
     }
 
-    const currentPosts = dataByActiveState?.slice(firstPostIndex, lastPostIndex)
+    console.log("firstPostIndex: ", firstPostIndex);
+    console.log("firstPostIndex: ", lastPostIndex);
+    let currentPosts = dataByActiveState
+    if (dataByActiveState?.length > 6) {
+      currentPosts = dataByActiveState?.slice(firstPostIndex, lastPostIndex);
+    }
+    console.log("CurrentPosts:", currentPosts);
 
-    setFilteredFranchises(currentPosts)
+    //setFilteredFranchises(currData);
+    setFilteredFranchises(currentPosts);
     //setFilteredFranchises(dataByActiveState)
-  }, [activeState, inputValue, currentPage])
+  }, [activeState, inputValue, currentPage]);
 
   return (
     <>
       <div>
-        <ul className='d-flex flex-row justify-content-center active-filter'>
-          <li onClick={handleClick} className="filter-active" id="all">All</li>
-          <li onClick={handleClick} className="" id="active">Active</li>
-          <li onClick={handleClick} className="" id="non-active">Non Active</li>
+        <ul className="d-flex flex-row justify-content-center active-filter">
+          <li onClick={handleFilterClick} className="filter-active" id="all">
+            Tout
+          </li>
+          <li onClick={handleFilterClick} className="" id="active">
+            Active
+          </li>
+          <li onClick={handleFilterClick} className="" id="non-active">
+            Non Active
+          </li>
         </ul>
       </div>
 
       <div className="input-group mb-3">
-        <input 
+        <input
           id="form-input"
-          type="search" 
-          className="form-control" 
-          placeholder="Entrez le(s) mot(s) clé(s)..." 
-          aria-label="Username" 
-          aria-describedby="basic-addon1" 
+          type="search"
+          className="form-control"
+          placeholder="Entrez le(s) mot(s) clé(s)..."
+          aria-label="Username"
+          aria-describedby="basic-addon1"
           onChange={handleInputField}
         />
-        <span className="input-group-text bg-primary text-white" id="basic-addon1"><i className="bi bi-search"></i></span>
+        <span
+          className="input-group-text bg-primary text-white"
+          id="basic-addon1"
+        >
+          <i className="bi bi-search"></i>
+        </span>
       </div>
-
+      <div>
+        <p>{errorOnLoading}</p>
+      </div>
       <section>
         <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
-        {
-          filteredFranchises?.map( (franchise, key) => {
-            return(
+          {filteredFranchises?.map((franchise, key) => {
+            return (
               <Card
                 key={key}
                 franchiseId={franchise.id}
@@ -126,25 +165,24 @@ const Franchises = () => {
                 franchiseImage={franchise.image}
                 franchiseDescription={franchise.description}
               />
-            )
-          })
-        }
+            );
+          })}
         </div>
       </section>
-      <nav className='pagination-section d-flex justify-content-center'>
-        {
-          filteredItemsQty > 6 ? (
-            <Pagination 
+      <nav className="pagination-section d-flex justify-content-center">
+        {filteredItemsQty > 6 ? (
+          <Pagination
             totalPosts={franchisesArray?.length}
             postsPerPage={postsPerPage}
             setCurrentPage={setCurrentPage}
             currentPage={currentPage}
           />
-          ) : ''
-        }
+        ) : (
+          ""
+        )}
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Franchises
+export default Franchises;
