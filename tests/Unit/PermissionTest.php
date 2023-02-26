14615ce9f22c5fb10d3e1a91bb868a7db7082e81
JuanJoseMora <juanmora.dev@gmail.com>
2023-02-26 09:34:17 +0100

    PermissionTest created

---
+++ tests/Unit/PermissionTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Entity\Permission;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Helper\Dumper;
+
+class PermissionTest extends KernelTestCase
+{
+  public function getEntity(): Permission
+  {
+    return (new Permission())
+      ->setName('abc')
+      ->setImage('');
+  }
+
+  public function testEntityIsValid(): void
+  {
+    self::bootKernel();
+    $container = static::getContainer();
+
+    $permission = $this->getEntity();
+
+    $errors = $container->get('validator')->validate($permission);
+
+    $this->assertCount(0, $errors);
+  }
+
+  public function testUrlIsNotValid(): void
+  {
+    $urlsArray = [
+      'http//urlnotvalid',  //NOT VALID
+      'https:/urlnotvalid', //NOT VALID
+      'urlnotvalid'         //NOT VALID
+    ];
+
+    self::bootKernel();
+    $container = static::getContainer();
+
+    $permission = $this->getEntity();
+
+    for ($i = 0; $i < count($urlsArray); $i++) {
+      $permission->setImage($urlsArray[$i]);
+      $errors = $container->get('validator')->validate($permission);
+      $this->assertCount(1, $errors);
+    }
+  }
+
+  public function testUrlIsValid(): void
+  {
+    $urlsArray = [
+      '',                   //VALID
+      'http://urlIsvalid',  //VALID
+    ];
+
+    self::bootKernel();
+    $container = static::getContainer();
+    $permission = $this->getEntity();
+    for ($i = 0; $i < count($urlsArray); $i++) {
+      $permission->setImage($urlsArray[$i]);
+      $errors = $container->get('validator')->validate($permission);
+      $this->assertCount(0, $errors);
+    }
+  }
+
+}
